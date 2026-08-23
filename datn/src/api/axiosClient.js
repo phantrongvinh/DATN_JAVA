5feb9df416953e279a2ffa8be0d788eee3229281
phantrongvinh <1phantrongvinh98@gmail.com>
2026-08-23 16:13:00 +0700

    corrs

--- datn/src/api/axiosClient.js
+++ datn/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'https://sports-ecommerce-production.up.railway.app/api/v1',
   responseType: 'json',
   paramsSerializer: {
     indexes: null,
